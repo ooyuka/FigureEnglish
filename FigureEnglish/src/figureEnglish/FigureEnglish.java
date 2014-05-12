98aa1e8f2a60a4dd95164791a6b7805ba16d1333
g12942oy <g12942oy@gm.tsuda.ac.jp>
2014-05-12 17:54:25 +0900

    99999まで変換できるようになった

--- FigureEnglish/src/figureEnglish/FigureEnglish.java
+++ FigureEnglish/src/figureEnglish/FigureEnglish.java
@@ -14,22 +14,50 @@ public class FigureEnglish {
     // 数値を英訳する変換するメソッド
     static String translateEng(int n) {
     	String s="";
+    	int n4 = n%10000;
     	int n3 = n%1000;
     	int n2 = n%100;
     	int nlast = n2%10;
     	
-    	if(n/1000!=0) {
-    		int n1000 = n/1000;
+    	if(n/10000!=0){
+    		int n10000 = n/10000;
+    		if(n10000==1) {
+    			int n1000 = n/1000;
+    			if(n1000==10) s+="ten ";
+        	    else if(n1000==11) s+="eleven ";
+        	    else if(n1000==12) s+="twelve ";
+        	    else if(n1000==13) s+="thirteen ";
+        	    else if(n1000==14) s+="fourteen ";
+        	    else if(n1000==15) s+="fifteen ";
+        	    else if(n1000==16) s+="sixteen ";
+        	    else if(n1000==17) s+="seventeen ";
+        	    else if(n1000==18) s+="eighteen ";
+        	    else if(n1000==19) s+="nineteen ";
+    		}
+    	    else if(n10000==2) s+="twenty ";
+    	    else if(n10000==3) s+="thirty ";
+    	    else if(n10000==4) s+="forty ";
+    	    else if(n10000==5) s+="fifty ";
+    	    else if(n10000==6) s+="sixty ";
+    	    else if(n10000==7) s+="seventy ";
+    	    else if(n10000==8) s+="eighty ";
+    	    else if(n10000==9) s+="ninety ";
+    	}
     	
-    		if(n1000==1) s+="one thousand ";
-    		else if(n1000==2) s+="two thousand ";
-    		else if(n1000==3) s+="three thousand ";
-    		else if(n1000==4) s+="four thousand ";
-    		else if(n1000==5) s+="five thousand ";
-    		else if(n1000==6) s+="six thousand ";
-    		else if(n1000==7) s+="seven thousand ";
-    		else if(n1000==8) s+="eight thousand ";
-    		else if(n1000==9) s+="nine thousand ";
+    	if(n4/1000!=0) {
+    		int n1000 = n4/1000;
+    		if(n/10000!=1) {
+    		if(n1000==1 && n/10000==0) s+="one ";
+    		else if(n1000==2) s+="two ";
+    		else if(n1000==3) s+="three ";
+    		else if(n1000==4) s+="four ";
+    		else if(n1000==5) s+="five ";
+    		else if(n1000==6) s+="six ";
+    		else if(n1000==7) s+="seven ";
+    		else if(n1000==8) s+="eight ";
+    		else if(n1000==9) s+="nine ";
+    		}
+    		s+="thousand ";
     	}
     	
     	if(n3/100!=0) {
@@ -80,6 +108,7 @@ public class FigureEnglish {
     	    else if(nlast==8) s+="eight";
     	    else if(nlast==9) s+="nine";
     	}
+    	else if(n<=100000) s+= "It is not a correct figure!";
 	    else s+="It is not a correct figure!";
     	return s;
     }
